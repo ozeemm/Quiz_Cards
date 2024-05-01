7afd26b1e0582c6ffa3470301b06eae5cdfe0754
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-05-01 21:43:29 +0800

    Added postgres maven driver

--- src/main/java/com/ozeemm/quizcards/QuizcardsApplication.java
+++ src/main/java/com/ozeemm/quizcards/QuizcardsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuizcardsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuizcardsApplication.class, args);
 	}
-
 }
