9b0c719427ccb4f912d93f1726e80df04e4a33e4
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-06-10 11:11:38 +0800

    Added name, description, version to SwaggerConfig

--- src/main/java/quiz/cards/backend/Config/SwaggerConfig.java
+++ src/main/java/quiz/cards/backend/Config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package quiz.cards.backend.Config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,5 +15,10 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer"
 )
 public class SwaggerConfig {
-
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info().title("Quiz Cards Backend")
+                .description("Testing API of backend for Quiz Cards")
+                .version("1.0.0"));
+    }
 }
